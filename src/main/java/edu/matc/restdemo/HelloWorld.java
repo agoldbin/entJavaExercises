1da994118a9ff1b2cf4734721112c9686cd3956f
Aaron Goldbin <agoldbin@madisoncollege.edu>
2018-04-01 22:31:58 -0500

    no idea what im missing

--- src/main/java/edu/matc/restdemo/HelloWorld.java
+++ src/main/java/edu/matc/restdemo/HelloWorld.java
@@ -1,7 +1,10 @@
 package edu.matc.restdemo;
 
-import javax.ws.rs.*;
-import  javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 
 @Path("/hello")
